8488da205d3186d2e606f9535d1dbcbcb5c99d82
Vincent_Ivan <vpalomata25@gmail.com>
2024-03-22 07:28:44 +0800

    sample api connection to openai (not usable)

---
+++ text_chatgpt.py
@@ -0,0 +1,26 @@
+import openai
+openai.api_key = "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"
+
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0
+    )
+    return response.choices[0].message.content
+
+
+user = input(">>> ")
+strip_text = user.split(">>> ")
+
+def convert_to_text(stripped_text):
+    raw = ""
+
+    return raw.join(stripped_text)
+
+
+result = get_completion(convert_to_text(strip_text))
+print(result)
